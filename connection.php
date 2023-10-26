dbd520295b7b128ada82860a3c9e86b438a84f05
Tăng Minh Nhựt <nhuttmgcc200149@fpt.edu.vn>
2023-10-26 13:30:52 +0700

    Update connection

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysqli_connect('localhost','root','','online_shopping')
+$conn = mysqli_connect('bv2rebwf6zzsv341.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','baz0v301el62kgfx','bzhsnyq5b8rt082n','wm9h41hyeohnt843')
  or die("Can not connect database".mysqli_connect_error());
 ?>
